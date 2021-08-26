08a15a9b341843851b1c44db1d2e7d83551c2cfd
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-08-26 03:18:44 -0300

    Mecanica de Salto del Player

--- Assets/Script/Controllers/PlayerController.cs
+++ Assets/Script/Controllers/PlayerController.cs
@@ -1,79 +1,175 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField]private float maxSpeed;
+    #region Serialize Fields
+
+    [Header("Movement")]
+    [SerializeField] private float speed; // Esta es la que se llamaba maxSpeed, pero el nombre no era descriptivo
     [SerializeField] private float sprintMultiplyer;
+
+    [Header("Rotation")]
+    [SerializeField] private Vector3 rotationSensibility;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpImpulseForce; // El impulso que se le dara al saltar
+
+    [Header("Attack")]
+    [SerializeField] private MachineGun weapon;
+
+    #endregion
+
+    #region Private Fields
+
+    // Components
     private Animator animator;
     private Rigidbody rigidBody;
+
+    // Movement
     private bool canMove;
-    [SerializeField] private Vector3 rotationSensibility;
+    private float currentSpeed; // Esta es la que antes se llamaba speed, pero no tiene sentido que sea serializada si en el Start se le pisa el valor
+
+    // Rotation
+    private bool canRotate;
     private float rotX;
     private float rotY;
-    [SerializeField] private MachineGun weapon;
-    // Start is called before the first frame update
-    void Start()
+
+    // Jump
+    private bool canJump; // Si tiene permitido saltar
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
     {
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
-        speed = maxSpeed;
+        currentSpeed = speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (canMove)
         {
-        Move();
+            Move();
+            Sprint();
         }
 
-        rotX += Input.GetAxis("Mouse X") * Time.deltaTime * rotationSensibility.x;
-        rotY += Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSensibility.y;
-        rotY = Mathf.Clamp(rotY, -20, 20);
-        if(rotX >= 360)
+        if (canRotate)
         {
-            rotX = 0;
+            Rotate();
         }
-        transform.rotation = Quaternion.Euler(new Vector3(-rotY, rotX,0 ));
-        Sprint();
+
+        if (canJump)
+        {
+            Jump();
+        }
+        else
+        {
+            CheckGround();
+        }
     }
-    
-    void Move()
+
+    #endregion
+
+    #region Private Methods
+
+    private void Move()
     {
         var moveX = Input.GetAxis("Horizontal");
         var moveZ = Input.GetAxis("Vertical");
-        Vector3 movement = transform.right * moveX + transform.forward * moveZ; 
+
+        Vector3 movement = transform.right * moveX + transform.forward * moveZ;
+
         if (canMove)
         {
-        transform.position += movement * speed * Time.deltaTime;
-
+            transform.position += movement * currentSpeed * Time.deltaTime;
         }
+
         animator.SetFloat("Speed", moveZ);
         animator.SetFloat("HorizontalSpeed", moveX);
     }
-    public void SetCanMove(bool enableMovement)
-    {
-        canMove = enableMovement;
-    }
-    public void WeaponShoot()
-    {
-        weapon.Shoot();
-    }
+
     private void Sprint()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            speed = speed * sprintMultiplyer;
+            currentSpeed = currentSpeed * sprintMultiplyer;
             animator.speed *= 2;
-                
+
         }
+
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            speed = maxSpeed;
+            currentSpeed = speed;
             animator.speed /= 2;
         }
     }
 
+    private void Rotate()
+    {
+        rotX += Input.GetAxis("Mouse X") * Time.deltaTime * rotationSensibility.x;
+        rotY += Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSensibility.y;
+        rotY = Mathf.Clamp(rotY, -20, 20);
+
+        if (rotX >= 360)
+        {
+            rotX = 0;
+        }
+
+        transform.rotation = Quaternion.Euler(new Vector3(-rotY, rotX, 0));
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            canJump = false;
+            var jumpForce = transform.up * jumpImpulseForce;
+            rigidBody.AddForce(jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void CheckGround()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, out hit, .2f))
+        {
+            if (hit.collider != null)
+            {
+                canJump = true;
+            }
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void SetCanMove(bool enableMovement)
+    {
+        canMove = enableMovement;
+    }
+
+    public void SetCanRotate(bool value)
+    {
+        canRotate = value;
+    }
+
+    public void SetCanJump(bool value)
+    {
+        canJump = value;
+    }
+
+    public void WeaponShoot()
+    {
+        weapon.Shoot();
+    }
+
+    #endregion
 }
