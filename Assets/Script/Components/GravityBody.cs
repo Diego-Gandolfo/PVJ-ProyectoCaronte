7c9100909aa370a71bb935cf869c82f4a178cf72
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-09-07 17:13:26 -0300

    Optimizacion de Gravity

--- Assets/Script/Components/GravityBody.cs
+++ Assets/Script/Components/GravityBody.cs
@@ -7,7 +7,7 @@ public class GravityBody : MonoBehaviour
 {
     #region Serialize Fields
 
-    [SerializeField] private GravityAttractor _attractor;
+    [SerializeField] private GravityAttractor _gravityAttractor;
 
     #endregion
 
@@ -20,18 +20,38 @@ public class GravityBody : MonoBehaviour
     #region Propertys
 
     public Rigidbody Rigidbody => _rigidbody;
-    public GravityAttractor Attractor => _attractor;
+    public GravityAttractor GravityAttractor => _gravityAttractor;
 
     #endregion
 
     #region Unity Methods
 
-    private void Start()
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.useGravity = false;
-        _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
-        if (_attractor != null) _attractor.AddGravityBody(this);
+    }
+
+    private void Start()
+    {
+        if (_gravityAttractor != null) AssignAttractor(_gravityAttractor);
+        else Debug.LogWarning($"{gameObject.name} tiene el Componente 'GravityBody' pero no tiene asignado un Gravity Attractor.");
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void AssignAttractor(GravityAttractor gravityAttractor)
+    {
+        if (_gravityAttractor != null) // si ya tiene un GravityAttractor
+            _gravityAttractor.RemoveGravityBody(this); // que lo saque de la lista
+
+        _gravityAttractor = gravityAttractor; // le asignamos el nuevo GravityAttractor
+
+        _gravityAttractor.AddGravityBody(this); // lo agregamos a la lista
+
+        _rigidbody.useGravity = false; // deshabilitamos la gravedad del Rigidbody
+        _rigidbody.constraints = RigidbodyConstraints.FreezeRotation; // freezeamos la rotacion del Rigidbody
     }
 
     #endregion
