61a78a5462f0185edfe82ca18a219637c35df403
Huergacho <75048402+Huergacho@users.noreply.github.com>
2021-09-11 01:56:49 -0300

    Crosshair UNfixed

--- Assets/Script/MachineGun.cs
+++ Assets/Script/MachineGun.cs
@@ -8,6 +8,7 @@ public class MachineGun : MonoBehaviour
     [SerializeField] private Transform firePoint;
     [SerializeField] private PlayerController owner;
     public Animator animator;
+   [SerializeField]private Transform crossHair;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,7 +35,32 @@ public class MachineGun : MonoBehaviour
     }
     public void Shoot()
     {
+        //Vector3 pos;
+        //pos.x = Input.mousePosition.x;
+        //pos.y = Input.mousePosition.y;
+        //pos.z = Vector3.Distance(firePoint.position, crossHair.transform.position);
+        //pos = Camera.main.ScreenToWorldPoint(pos);
 
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        //var bulletClone = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        //bulletClone.transform.forward = firePoint.position - pos;
+
+        //RaycastHit hit;
+
+        //if(Physics.Raycast(crossHair.position, crossHair.forward, out hit, Mathf.Infinity))
+        //{
+        //    var bulletClone = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        //    print(hit.point);
+        //    bulletClone.transform.forward = Vector3.Distance(hit.point, firePoint.position) > 0.5f ? (hit.point - firePoint.position) : bulletClone.transform.forward;
+        //}
+
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Camera.main.transform.forward);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            var bulletClone = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            print(hit.point);
+            bulletClone.transform.forward = firePoint.position - hit.point;
+        }
     }
 }
