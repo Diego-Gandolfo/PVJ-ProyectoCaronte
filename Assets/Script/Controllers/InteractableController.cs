dbded3310422c808468a43c3c6ed16ad325b65f7
Huergacho <75048402+Huergacho@users.noreply.github.com>
2021-09-12 17:56:11 -0300

    Zona segura agregada
    + Zona segura
    + Shader Package
    + Interactable Object New Void

--- Assets/Script/Controllers/InteractableController.cs
+++ Assets/Script/Controllers/InteractableController.cs
@@ -7,11 +7,13 @@ public class InteractableController : MonoBehaviour
     public IInteractable interactable;
     private Outline outline;
     private bool canInteract;
+    private bool hasInteractedOnce;
 
     private void Start()
     {
         outline = GetComponent<Outline>();
         outline.enabled = false;
+        hasInteractedOnce = false;
     }
 
     private void Update()
@@ -24,29 +26,42 @@ public class InteractableController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 6) //Si es un player
+        if (!hasInteractedOnce)
         {
-            HUDManager.instance.ShowPrompt(true);
-            outline.enabled = true;
-            canInteract= true;
+            if (other.gameObject.layer == 6) //Si es un player
+            {
+                HUDManager.instance.ShowPrompt(true);
+                outline.enabled = true;
+                canInteract = true;
+            }
         }
+
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer == 6)
+        if (!hasInteractedOnce)
         {
-            HUDManager.instance.ShowPrompt(false);
-            outline.enabled = false;
-            canInteract = false;
+            if (other.gameObject.layer == 6)
+            {
+                HUDManager.instance.ShowPrompt(false);
+                outline.enabled = false;
+                canInteract = false;
+            }
         }
+
     }
 
-    private void Interact()
+    public  void Interact()
     {
         HUDManager.instance.ShowPrompt(false); //Le sacamos el prompt 
         outline.enabled = false; //apagamos el outline
 
         interactable.Interact(); //Acá es donde cada interactable hace los suyo  
+    }
+    public void ActivateOnce()
+    {
+        hasInteractedOnce = true; //si solo queremos activarlo una sola vez llamamos a esta funcion
+        canInteract = false;
     }
 }
