ba19dba345df5c756d6b7e30f30f754c5f23d459
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-09-17 02:11:01 -0300

    Camera Fix
    No es lo mas elegante, ni lo que más bonito queda... Pero ya no hace la cosa rara... Después si puedo les cuento algunas ideas que tengo para mejorarlo...

--- Assets/CameraCollisionDetection.cs
+++ Assets/CameraCollisionDetection.cs
@@ -1,3 +1,61 @@
+using UnityEngine;
+
+public class CameraCollisionDetection : MonoBehaviour
+{
+    #region Serialize Fields
+
+    [SerializeField] private LayerMask _hitLayers;
+    [SerializeField] private Vector3 _closePositionModifier;
+    [SerializeField] private float _closeFOVModifier;
+
+    #endregion
+
+    #region Private Fields
+
+    private Vector3 _closePosition;
+    private float _closeFOV;
+    private Vector3 _originalPosition;
+    private float _originalFOV;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        _originalPosition = transform.localPosition;
+        _closePosition = _originalPosition + _closePositionModifier;
+
+        _originalFOV = Camera.main.fieldOfView;
+        _closeFOV = _originalFOV + _closeFOVModifier;
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+
+        if (!Physics.Linecast(transform.position, transform.forward, out hit, _hitLayers))
+        {
+            transform.localPosition = _closePosition;
+            Camera.main.fieldOfView = _closeFOV;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.position, transform.forward, out hit, _hitLayers))
+        {
+            transform.localPosition = _originalPosition;
+            Camera.main.fieldOfView = _originalFOV;
+        }
+    }
+
+    #endregion
+}
+
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,3 +74,4 @@ public class CameraCollisionDetection : MonoBehaviour
         }
     }
 }
+*/
