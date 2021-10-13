3a8111aea920664f09db2e8f9690fd280d62dff5
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-10-13 06:36:04 -0300

    TinyMeleeEnemy

---
+++ Assets/Script/Components/AnimatorSpeedChanger.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class AnimatorSpeedChanger : MonoBehaviour
+{
+    #region Serialized Fields
+
+    [SerializeField] private float _animatorSpeed = 1;
+
+    #endregion
+
+    #region Private Fields
+
+    // Components
+    private Animator _animator;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        if (_animator != null)
+        {
+            _animator.speed = _animatorSpeed;
+        }
+        else 
+        {
+            Debug.LogError($"{this}: {gameObject.name} no tiene un Animator");
+        }
+    }
+
+    #endregion
+}
