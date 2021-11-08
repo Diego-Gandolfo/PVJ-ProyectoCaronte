3dee5c67fc75aa0279df8da978486fe264c8b00e
Jessica <jessica.galvan@hotmail.com>
2021-11-08 19:06:25 -0300

    CoolingSystem WIP

---
+++ Assets/Script/UI/ShopItems/CoolingSystemItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolingSystemItem : BaseItemShop
+{
+    public override void Interact()
+    {
+        //TODO: hacer anti venom mask upgrade
+    }
+}
