4bbcf626540aa72f6b53de4f1bf361c4797ef638
Jessica <jessica.galvan@hotmail.com>
2021-11-08 18:47:16 -0300

    Upgraded WIP

---
+++ Assets/Script/UI/ShopItems/OxygenUpgradeItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenUpgradeItem : BaseItemShop
+{
+    public override void Interact()
+    {
+        //TODO: hacer oxygen upgrade
+    }
+}
